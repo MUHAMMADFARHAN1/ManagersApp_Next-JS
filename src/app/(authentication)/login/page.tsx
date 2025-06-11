7e66c62ca56137f05e6881c000db09ac5c0529eb
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-11 11:19:25 +0200

    repo refactored!

---
+++ src/app/(authentication)/login/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import coding from "./../assets/coding.png";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <div className="grid grid-cols-2 h-screen">
+      <div className=" bg-[#23155b] ">
+        <Image src={coding} className="m-auto pt-35" alt="" />
+      </div>
+      <div className=" bg-[#f4f4f4] ">
+        <div className=" bg-white w-1/3 flex flex-col mx-auto mt-40 text-center gap-2 rounded py-8 px-2 min-w-min">
+          <p className="pb-4">Join Coders Now!</p>
+          <form
+            // onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-2"
+          >
+            <input
+              placeholder="Email"
+              className=" bg-[#23155b]  rounded py-1 text-white placeholder:p-1"
+            />
+            <input
+              className=" bg-[#23155b]  rounded py-1 text-white placeholder:p-1"
+              type="text"
+              placeholder="Password"
+            />
+            <button type="submit" className="bg-blue-600 rounded">
+              Login
+            </button>
+          </form>
+          <div>
+            <p>
+              New to CodeCLA? <Link href="/">Signup</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

cf9cbd8daa67e65a3ce8963bf9a2938f560cd31e
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-12 16:33:56 +0200

    Basic Table Created!

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,40 @@
 import React from "react";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import Menu from "../../../components/menu";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function page() {
   return (
     <div>
       <Menu />
-      <p className="text-center">
-        This is a site i cretaed as Next.js tutorial to be used later on.
-      </p>
+      <Table>
+        <TableCaption>Your challenges list</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Title</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Difficulty</TableHead>
+            <TableHead>Created at</TableHead>
+            <TableHead className="text-left">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }

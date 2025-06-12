3731f335b646012583b637d12d58328a78f225ad
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-12 14:43:50 +0200

    zod integration completed!

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,53 +1,76 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 export default function page() {
+  const FormSchema = z.object({
+    firstname: z.string().min(6, "First name must be atleast 2 characters"),
+    lastname: z.string().min(6, "First name must be atleast 2 characters"),
+    email: z
+      .string()
+      .email("Invalid email. Email must be a valid email address"),
+    password: z
+      .string()
+      .min(6, "Password must not be lesser than 6 characters"),
+  });
+
+  type IFormInput = z.infer<typeof FormSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data: IFormInput) => {
+    console.log(data);
+  };
+
   return (
     <div className=" bg-white w-1/3 flex flex-col mx-auto mt-40 text-center gap-2 rounded py-8 px-2 min-w-min">
       <p className="pb-4">Join Coders Now!</p>
       <form
         action=""
         className="flex flex-col gap-2"
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <input
-          type="text"
-          name="First Name"
+          {...register("firstname")}
           placeholder="First Name"
-          //   value={First}
-          //   onChange={(e) => setFirstName(e.target.value)}
           className=" bg-[#23155b] rounded py-1 text-white placeholder:p-2"
         />
+        {errors?.firstname?.message && (
+          <p className="text-red-700 mb-4">{errors.firstname.message}</p>
+        )}
         <input
-          type="text"
-          name="Last Name"
+          {...register("lastname")}
           placeholder="Last Name"
-          //   value={Last}
-          //   onChange={(e) => setLastName(e.target.value)}
           className=" bg-[#23155b] rounded py-1 text-white placeholder:p-2"
         />
+        {errors?.lastname?.message && (
+          <p className="text-red-700 mb-4">{errors.lastname.message}</p>
+        )}
         <input
-          type="text"
-          name="Email"
+          {...register("email")}
           placeholder="Email"
-          //   value={Mail}
-          //   onChange={(e) => setEmail(e.target.value)}
           className=" bg-[#23155b] rounded py-1 text-white placeholder:p-2"
         />
-        {/* {errorMessageMail && (
-              <div className="error">{errorMessageMail}</div>
-            )} */}
+        {errors?.email?.message && (
+          <p className="text-red-700 mb-4">{errors.email.message}</p>
+        )}
         <input
-          type="text"
-          name="Password"
+          {...register("password")}
           placeholder="Password"
-          //   value={Pass}
-          //   onChange={(e) => setPassword(e.target.value)}
           className=" bg-[#23155b] rounded py-1 text-white placeholder:p-2"
         />
-        {/* {errorMessagePass && (
-              <div className="error">{errorMessagePass}</div>
-            )} */}
+        {errors?.password?.message && (
+          <p className="text-red-700 mb-4">{errors.password.message}</p>
+        )}
         <button type="submit" className="bg-blue-600">
           Sign Up
         </button>

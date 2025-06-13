b34fc6c4476f4bcb47985d458b195ebf158e6143
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-13 13:28:52 +0200

    button added!

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import Menu from "../../../components/menu";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import {
   Table,
   TableBody,
@@ -10,6 +12,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Trash2, Edit } from "lucide-react";
 
 export async function getChallenges() {
   const res = await fetch("http://localhost:2000/challenges");
@@ -24,34 +27,42 @@ export default async function page() {
   const challenges = await getChallenges();
   console.log(challenges);
   return (
-    <div>
-      <Table>
-        <TableCaption>Your challenges list</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Title</TableHead>
-            <TableHead>Category</TableHead>
-            <TableHead>Difficulty</TableHead>
-            <TableHead>Created at</TableHead>
-            <TableHead className="text-left">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {challenges.map((challenges: any) => (
+    <>
+      <p>Your challenges</p>
+      <Button>
+        <Link href="/Challenge">New Challenge</Link>
+      </Button>
+      <div>
+        <Table>
+          <TableCaption>Your challenges list</TableCaption>
+          <TableHeader>
             <TableRow>
-              <TableCell className="font-medium">{challenges.title}</TableCell>
-              <TableCell className="font-light">
-                {challenges.category}
-              </TableCell>
-              <TableCell className="font-light">{challenges.level}</TableCell>
-              <TableCell className="font-light">
-                {challenges.createdAt}
-              </TableCell>
+              <TableHead className="w-[100px]">Title</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Difficulty</TableHead>
+              <TableHead>Created at</TableHead>
+              <TableHead className="text-left">Actions</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {challenges.map((challenges: any) => (
+              <TableRow>
+                <TableCell className="font-medium">
+                  {challenges.title}
+                </TableCell>
+                <TableCell className="font-light">
+                  {challenges.category}
+                </TableCell>
+                <TableCell className="font-light">{challenges.level}</TableCell>
+                <TableCell className="font-light">
+                  {challenges.createdAt}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 }
 
